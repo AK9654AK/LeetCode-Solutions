48ab0991ac0e6231dae53ba035222afe847989fc
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-01 21:49:33 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (76.63%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        
+        if(n<0)return false;
+        
+        int count=0;
+        while(n){
+            
+            n=n&(n-1);
+            count++;
+            
+            
+            
+        }
+        
+        if(count==1){
+            return true;
+        }
+        else return false;
+        
+        
+    }
+};
