4f847615ca47f505c7876a65dde46214b640b92e
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-03 19:01:30 +0530

    Time: 13 ms (65.73%), Space: 13 MB (20.83%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -4,19 +4,15 @@ public:
         
         
         
-        vector<int> ans;
         int n=nums.size();
+        // nums.resize(2);
         
         
         for(int i=0;i<n;i++){
-            ans.push_back(nums[i]);
-        }
-        for(int i=0;i<n;i++){
-            ans.push_back(nums[i]);
-
+            nums.push_back(nums[i]);
         }
         
-        return ans;
+        return nums;
         
         
         
