13e897786d3611f09cbf387b71bd0588725a64fa
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-05 22:49:06 +0530

    Update 0771-jewels-and-stones.cpp

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -31,7 +31,8 @@ public:
 //Another aproach to save space of the hash map;
 // int numJewelsInStones(string jewels, string stones) {
 //         int cnt = 0;
-//         vector<int> hash(100, 0);   //can be changed to 58
+//         vector<int> hash(100, 0);   //can be changed to 58In the ASCII table, there are 6 positions reserved 
+//                                       for symbols between the sets of uppercase and lowercase alphabet. 26*2+6=58. 
         
 //         for(char it : jewels)
 //             hash[(int)(it-65)]++;
