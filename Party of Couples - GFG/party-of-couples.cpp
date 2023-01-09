3a6a0bb80fae97056e2872da413c73caf2d23b10
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-09 20:13:11 +0530

    Added solution - LeetHub

---
+++ Party of Couples - GFG/party-of-couples.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int findSingle(int N, int arr[]){
+        // code here
+       
+       
+       int ans=0;
+       for(int i=0;i<N;i++)ans^=arr[i];
+       return ans;
+        
+        // int hash[10000]={0};
+        
+        // for(int i=0;i<N;i++)hash[arr[i]]++;
+        // for(int i=0;i<10000;i++)if(hash[i]==1)return i;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, X;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.findSingle(N, arr) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
