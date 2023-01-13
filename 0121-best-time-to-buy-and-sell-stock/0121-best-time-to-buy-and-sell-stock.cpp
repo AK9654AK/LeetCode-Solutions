12584d204659e435d52a5bf530d17c9ec464d185
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-13 22:05:13 +0530

    Time: 137 ms (84.75%), Space: 93.3 MB (54.34%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+       
+        int profit=0;
+        
+        int minimum=prices[0];
+        
+        for(int i=1;i<prices.size();i++){ 
+            
+            
+            
+            ///https://www.youtube.com/watch?v=eMSfBgbiEjk
+            
+            
+            
+            
+            
+
+            
+            
+            
+            ////////////////The idea is quite simple, we’ve to find the max profit by selling the stocks.
+// We took 2 int variables: mn (to store the minimum till a particular date) & ans.
+// Run a for loop and in every time check for the minimum value till that iteration and store it in mn ~min(mn, prices[i]).
+// Also, find the ans with max(ans, prices[i]-mn).
+// Time complexity: O(n).
+            
+            
+            
+            
+           minimum=min(prices[i],minimum);
+            profit=max(profit,(prices[i]-minimum));
+        }
+        return profit;
+    }
+};
