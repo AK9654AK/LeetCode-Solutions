ce644a909f2aca73a5c0171d21eb84d6504c4643
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-19 16:23:08 +0530

    Added solution - LeetHub

---
+++ Anagram of String - GFG/anagram-of-string.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+const int CHARS = 26;
+
+// function to calculate minimum numbers of characters
+// to be removed to make two strings anagram
+int remAnagram(string str1, string str2);
+
+// Driver program to run the case
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string str1,str2;
+        cin>>str1>>str2;
+        cout << remAnagram(str1, str2);
+        cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+// function to calculate minimum numbers of characters
+// to be removed to make two strings anagram
+int remAnagram(string str1, string str2)
+{
+    
+    
+    int hash[26] = {0};
+    
+    for(int i=0;i<str1.size();i++)
+    hash[str1[i]-'a']++;
+    
+    for(int i=0;i<str2.size();i++)
+    hash[str2[i]-'a']--;
+    
+    int ans = 0;
+    
+    for(int i=0; i<26; i++)
+        if(hash[i] != 0)
+            ans += abs(hash[i]);
+            
+    return ans;
+}
