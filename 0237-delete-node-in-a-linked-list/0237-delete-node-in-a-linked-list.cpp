6a274dea924116c26ee813625b23277379fc1c96
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-21 23:42:01 +0530

    Update 0237-delete-node-in-a-linked-list.cpp

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -11,6 +11,33 @@ public:
     void deleteNode(ListNode* node) {
      
         
+        
+         
+//         //1st method
+//         //we don't delete the node we move it forward
+//         *node=*node->next; 
+        
+        
+//         //2nd method;
+//         //we copy the value of next varibale and move the pointer to next
+//         node->val=node->next->val;
+//         node->next=node->next->next;
+        
+//         //3rd method
+//         //we copy the value of next varibale and move the pointer to next
+//         //also remove the node so to escape memory leak
+//         struct ListNode *temp=node->next;
+//         node->val=node->next->val;
+//         node->next=node->next->next;
+//         delete temp;
+        
+        
+        
+        
+        
+        
+        
+        
         ListNode *temp=node->next;
         node->val=node->next->val;
         node->next=node->next->next;
