950716e704d4ed1d8f8c96034a65f2ff8665ed29
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-21 20:35:47 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (27.88%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -19,7 +19,63 @@ public:
             count++;
             temp=temp->next;
         }
-        // if(count%2==0)count+1;
+        
+        
+        
+        
+        
+        
+//         class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode *temp = head;
+//         int length=0;
+// 				//Traversing temp till the end of linked list to find length #O(n)
+//         while(temp){
+//             length++;
+//             temp = temp->next;
+//         }
+//         int middle = length/2;
+//         temp=head;
+//        //traversing temp node back to middle node #O(n/2)
+//         while(temp && middle--)
+//             temp=temp->next;
+        
+//         return temp;
+//     }
+// };
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         lass Solution {
+// public:
+//         ListNode* middleNode(ListNode* head) {
+//         ListNode *slow = head, *fast = head;
+//         while (fast && fast->next)
+//             slow = slow->next, fast = fast->next->next;
+//         return slow;
+//     }
+// };
+        
+        
+        
+        
+        
+        
         
         
         for(int i=0;i<count/2;i++){
