d00f69b0846be1fe3945068d9648e54387b7b9ff
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-26 03:43:34 +0530

    Added solution - LeetHub

---
+++ Roof Top - GFG/roof-top.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find maximum number of consecutive steps 
+    //to gain an increase in altitude with each step.
+    int maxStep(int A[], int N)
+    {  int count=0;
+       int ans=0;
+      if(N==1)return 0;
+       for(int i=0;i<N-1;i++){
+           
+           if(A[i+1]>A[i])count++;
+           else count=0;
+           
+           ans=max(ans,count);
+       }
+       
+       return ans;
+       
+       
+       
+       
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	    cin>>a[i];
+	    Solution ob;
+	    cout << ob.maxStep(a, n) << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
