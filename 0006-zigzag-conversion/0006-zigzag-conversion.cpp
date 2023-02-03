c66ea7550848f0d33b387b4e8a8e67555e0b1ecc
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-02-03 19:25:24 +0530

    Time: 20 ms (46.60%), Space: 10.8 MB (41.51%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        
+        int n=numRows;
+       if(n<=1)return s;
+        // string str="";
+        vector<string>str(numRows,"");
+        int row=0;int step=1;
+        for(int i=0;i<s.size();i++){
+            str[row]+=s[i];
+            if(row==0)step=1;
+            if(row==n-1)step=-1;
+            row+=step;
+        }
+        string abhi;
+        for(int i=0;i<str.size();i++) abhi+=str[i];
+        return abhi;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int n=numRows;
+//         string s1;string s2;string s3;
+//         int ptr=0;
+//         int i=0;
+//         while(i<s.length()){
+//             ptr=ptr%n;
+            
+//             if(ptr==0){
+//                 s1+=s[i++];
+//                 ptr++;
+//             }
+//             if(ptr==1){
+//                 s2+=s[i++];
+//                 ptr++;
+//             }
+//             if(ptr==2){
+//                 s3+=s[i++];
+//                 ptr++;
+//             }
+            
+            
+//         }
+        
+//         string abhi=s1+s2+s3;
+            
+//         return abhi;
+            
+            
+            
+            
+        }
+     
+    
+    
+    
+    
+};
