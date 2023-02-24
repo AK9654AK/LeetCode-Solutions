1e38edc4cd6168f893861348fb8ecf1d28054389
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-02-24 22:33:33 +0530

    Added solution - LeetHub

---
+++ Sort in specific order - GFG/sort-in-specific-order.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    void sortIt(long long arr[], long long n)
+    {
+        for(int i=0;i<n;i++)if(arr[i]%2)arr[i]=(-1)*arr[i];
+       sort(arr,arr+n);
+       int i=0;
+       while(arr[i]<0){
+           arr[i] = (-1)*arr[i];
+           i++;
+       }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long arr[n];
+
+        for (int i = 0; i < n; i++) 
+            cin >> arr[i];
+        
+        Solution ob;
+        ob.sortIt(arr, n);
+
+        for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
