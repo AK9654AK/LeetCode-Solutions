9d45679451d5334f67d3867706fc6fb72f7efe87
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-03-07 19:55:50 +0530

    Added solution - LeetHub

---
+++ Delete Middle of Linked List - GFG/delete-middle-of-linked-list.cpp
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to get the middle of the linked list*/
+struct Node *deleteMid(struct Node *head);
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		head = deleteMid(head);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+
+
+// } Driver Code Ends
+
+
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+Node* deleteMid(Node* head)
+{
+    
+    Node *curr=head; int c=0;
+    
+    while(curr){
+        c++;
+        curr=curr->next;
+    }
+    
+    // if(c%2==0){
+     
+     
+     Node *del=head;
+     for(int i=1;i<c/2;i++){
+         del=del->next;
+     }
+        Node *temp=del->next;
+        
+        del->next=del->next->next;
+        delete temp;
+        return head;
+        
+    
+    // else{
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
