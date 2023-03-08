64cbae69f61279354ce9c0e1d7879ebdb44c561b
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-03-09 01:29:54 +0530

    Added solution - LeetHub

--- Max min Height - GFG/max-min-height.cpp
+++ Max min Height - GFG/max-min-height.cpp
@@ -12,13 +12,14 @@ class Solution
     public:
  
     bool possible(int n,vector<int> a,int k,int w,int mid){
-        for(int i=0;i<n;i++){
+          ////we cannt take it as by reference, since value will get changed which will result in altered ans.
+        for(int i=0;i<n;i++){ //for whole array 
             if(a[i]<mid){
-                int temp = mid-a[i];
-                if(temp>k)return false;
-                k-=temp;
+                int temp = mid-a[i];   // if height is less than mid, on which were checking.
+                if(temp>k)return false;  //if no. of days are not enought then return false.
+                k-=temp;   //if days left
                 a[i]=mid;
-                for(int j=i+1;j<n and j<i+w;j++){
+                for(int j=i+1;j<n and j<i+w;j++){   ///next plants i.e i+w plants will also be incresed.
                     a[j]+=temp;
                 }
             }
@@ -28,17 +29,22 @@ class Solution
     long long int maximizeMinHeight(int n,int k,int w,vector <int> &a)
     {
         // code here
-        int mn=INT_MAX,mx;
-        for(int i=0;i<n;i++)mn=min(mn,a[i]);
+        int mx;
+       // for(int i=0;i<n;i++)mn=min(mn,a[i]);
+       
+        int mn=*min_element(a.begin(),a.end()); //directly get the min element from the vector.
+        
         mx=mn+k;
+        
+        
         long long int ans;
         while(mn<=mx){
-            int mid = (mn+mx)/2;
-            if(possible(n,a,k,w,mid)){
-                ans=mid;
-                mn=mid+1;
+            int mid = (mn+mx)/2;  //to get the mid
+            if(possible(n,a,k,w,mid)){     //if possible to set the height of initial n flower, in k days daily w times against the mid days
+                ans=mid;           // if yes, set ans to mid
+                mn=mid+1;          //if yes, we will try too check for the large value also.
             }
-            else mx=mid-1;
+            else mx=mid-1;// otherwise decrease by 1
         }
         return ans;
     }
