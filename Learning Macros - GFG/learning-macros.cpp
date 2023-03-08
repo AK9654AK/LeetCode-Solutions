c8bd044fdc9fadd4b98e89f4415c8db43ea6c195
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-03-08 23:15:51 +0530

    Added solution - LeetHub

--- Learning Macros - GFG/learning-macros.cpp
+++ Learning Macros - GFG/learning-macros.cpp
@@ -16,7 +16,7 @@ void macros(int a,int b)
     //See the macro defined at the top of the code. Use that macro to find a*b
     //Use only macro to complete this task
     
-    cout<<a*b<<endl;
+    cout<<a*b<<endl;    //// Smoooooth tho.
 }
 
 //{ Driver Code Starts.
