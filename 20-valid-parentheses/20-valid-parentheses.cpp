0c4807544fad25a60abf950a61bef2602427bf00
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-05 22:15:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (97.14%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         
-        if(s.size()%2==1)return false;
+        // if(s.size()%2==1)return false;
         stack<char>st;
         
         
