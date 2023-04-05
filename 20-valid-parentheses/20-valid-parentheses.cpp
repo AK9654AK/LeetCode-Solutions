40674e737dcc85a83490e97fb6389471f0416a6b
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-05 22:14:53 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (81.44%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        if(s.size()%2==1)return false;
+        stack<char>st;
+        
+        
+        for(auto i:s){
+            if(i=='('||i=='['||i=='{')st.push(i);
+            else{
+                if(st.empty()) return false;
+                
+                if(i==')'){
+                    if(st.top()!='('){
+                        return false;
+                        
+                    }
+                }   ///Wrong output for "([}}])"
+
+                if(i=='}'){
+                    if(st.top()!='{'){
+                        return false;
+                      
+                }
+                }
+                if(i==']'){
+                    if(st.top()!='['){
+                       return false;
+                        
+                }   
+            }
+                 
+                st.pop();    // else if(st.empty()) return false;
+                }   
+                    
+        }
+         
+                 if(st.empty())return true;else return false;
+        
+        
+    }
+        
+        
+        
+        
+        
+//         bool isValid(string s) {
+//     if(s.size() % 2 == 1) return false;
+//     stack<char> st;
+//     for(auto i : s) {
+//         if(i == '(' || i == '[' || i == '{') st.push(i);
+//         else {
+//             if(st.empty()) return false;
+//             if(i == ')' && st.top() != '(') return false;
+//             if(i == '}' && st.top() != '{') return false;  //CHat GPT;
+//             if(i == ']' && st.top() != '[') return false;
+//             st.pop();
+//         }
+//     }
+//     return st.empty();
+// }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+};
