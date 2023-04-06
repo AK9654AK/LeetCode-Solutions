3aa4fd212764bfc58d49bb1a0e894d25f690bef5
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-06 22:54:13 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (99.52%) - LeetHub

---
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+       
+        long first=LONG_MIN;long sec=LONG_MIN;long third=LONG_MIN;
+        
+        
+        
+        for(auto i:nums){
+            
+            if(i==first||i==sec||i==third)continue;
+            
+            if(i>first){
+                third=sec;
+                sec=first;
+                first=i;
+            }
+            else if(i>sec){
+                third=sec;
+                sec=i;
+            }
+            else if(i>third)third=i;
+        
+        }
+        if(third==LONG_MIN)return first;
+        return third;
+    }
+};
