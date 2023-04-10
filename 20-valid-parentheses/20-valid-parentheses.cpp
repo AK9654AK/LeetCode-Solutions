6d8f129389de85cefaef48f2ea7e3f3217009ea8
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-10 10:58:49 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (81.54%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,64 +2,72 @@ class Solution {
 public:
     bool isValid(string s) {
         
-        // if(s.size()%2==1)return false;
-        stack<char>st;
         
         
-        for(auto i:s){
-            if(i=='('||i=='['||i=='{')st.push(i);
-            else{
-                if(st.empty()) return false;
-                
-                if(i==')'){
-                    if(st.top()!='('){
-                        return false;
-                        
-                    }
-                }   ///Wrong output for "([}}])"
-
-                if(i=='}'){
-                    if(st.top()!='{'){
-                        return false;
-                      
-                }
-                }
-                if(i==']'){
-                    if(st.top()!='['){
-                       return false;
-                        
-                }   
-            }
-                 
-                st.pop();    // else if(st.empty()) return false;
-                }   
-                    
+          
+    if(s.size() % 2 == 1) return false;
+    stack<char> st;
+    for(auto i : s) {
+        if(i == '(' || i == '[' || i == '{') st.push(i);
+        else {
+            if(st.empty()) return false;
+            if(i == ')' && st.top() != '(') return false;
+            if(i == '}' && st.top() != '{') return false;  
+            if(i == ']' && st.top() != '[') return false;
+            st.pop();
         }
-         
-                 if(st.empty())return true;else return false;
-        
-        
     }
+    return st.empty();
+}
         
         
         
         
         
-//         bool isValid(string s) {
-//     if(s.size() % 2 == 1) return false;
-//     stack<char> st;
-//     for(auto i : s) {
-//         if(i == '(' || i == '[' || i == '{') st.push(i);
-//         else {
-//             if(st.empty()) return false;
-//             if(i == ')' && st.top() != '(') return false;
-//             if(i == '}' && st.top() != '{') return false;  //CHat GPT;
-//             if(i == ']' && st.top() != '[') return false;
-//             st.pop();
+        
+//         if(s.size()%2==1)return false;
+//         stack<char>st;
+        
+        
+//         for(auto i:s){
+//             if(i=='('||i=='['||i=='{')st.push(i);
+//             else{
+//                 if(st.empty()) return false;
+                
+//                 if(i==')'){
+//                     if(st.top()!='('){
+//                         return false;
+                        
+//                     }
+//                 }   ///Wrong output for "([}}])"
+
+//                 if(i=='}'){
+//                     if(st.top()!='{'){
+//                         return false;
+                      
+//                 }
+//                 }
+//                 if(i==']'){
+//                     if(st.top()!='['){
+//                        return false;
+                        
+//                 }   
+//             }
+                 
+//                 st.pop();    // else if(st.empty()) return false;
+//                 }   
+                    
 //         }
+         
+//                  if(st.empty())return true;else return false;
+        
+        
 //     }
-//     return st.empty();
-// }
+        
+        
+        
+        
+ 
         
         
         
