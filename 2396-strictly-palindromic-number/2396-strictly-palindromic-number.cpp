e96676039a989a2fb24064418532d82dbfa8eaef
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-11 12:40:10 +0530

    Time: 0 ms (100.00%), Space: 6 MB (61.22%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+//     int num = 23;
+//     int base = 8;
+//     string binary_str = "";
+
+//     while(num > 0) {
+//         int rem = num % base;
+//         binary_str += to_string(rem);
+//         num /= base;
+//     }
+
+//     reverse(binary_str.begin(), binary_str.end());
+//     cout << binary_str << endl; // Output: 010111
+//     return 0;
+        
+        return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+};
