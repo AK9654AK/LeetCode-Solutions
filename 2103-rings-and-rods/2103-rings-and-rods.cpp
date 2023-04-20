0cf3aefcfd117d95bcfe11286c0d78ea6de9c102
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-20 11:07:23 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.86%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+        
+        bool red[10]={false}; bool blue[10]={false}; bool green[10]={false};
+        
+        for(int i=0;i<rings.size()-1;i+=2){
+            
+            if(rings[i]=='R'){
+                red[rings[i+1]-'0']=true;
+            }
+            if(rings[i]=='G'){
+                green[rings[i+1]-'0']=true;          
+            }
+            if(rings[i]=='B'){
+                blue[rings[i+1]-'0']=true;
+            }
+            
+        }
+        
+        int count=0;
+        
+        for(int i=0;i<10;i++){
+            if(blue[i] and green[i] and red[i])count++;
+        }
+        
+        
+        return count;
+        
+        
+//         map<int,string>mp;
+//         bool r,g,b;
+//         for(int i=0;i<rings.size();i+=2){
+//           mp[rings[i+1]]+=rings[i];
+//         }
+//         int count=0;
+//         for(auto x:mp){
+//             string temp=x.second;
+            
+//             r=false;g=false;b=false;
+//             for(auto t:temp){
+//                 if(t=='R')r=true;
+//                 if(t=='B')b=true;
+//                 if(t=='G')g=true;
+//                 if(r && g && b){count++;continue;}
+//             }
+//         }
+        
+//         return count;
+    }
+};
