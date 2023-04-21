c1652cf2a0a28beb1e6c323ef6c0982be050861a
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-22 01:01:39 +0530

    Time: 6 ms, Memory: 10 MB - LeetHub

---
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+       int i = 0;
+    int j = 0;
+    
+     while(i<arr.size()-1){
+         if(arr[i]==arr[j]*2&&i!=j&&i>=0){
+             return true;
+         }
+        
+         if(j<arr.size()-1){
+             j++;
+          } 
+
+        else {
+            i++;
+            j = 0;
+        }
+     }
+    return false;
+    
+    }
+};
