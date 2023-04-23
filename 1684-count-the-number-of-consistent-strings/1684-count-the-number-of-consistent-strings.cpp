adb64095ba63d8074eb352a8930c6fd224f31f51
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-23 15:55:50 +0530

    Time: 46 ms (96.50%), Space: 30.2 MB (36.08%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    
-    bool consis(string all, string word){
-        int hash[26]={0};
-        for(auto i:word)hash[i-'a']=1;
-        for(auto i: all)hash[i-'a']=0;
-        for(auto x:hash)if(x==1)return false;
-        return true;   
-    }
     int countConsistentStrings(string allowed, vector<string>& words) {
-        int count=0;
-        for(auto i:words){
-            if(consis(allowed,i)==true)count++;  
+        
+int mask=0;
+int ans=0;
+        
+for (int i=0;i<allowed.size();i++) {
+    mask|=(1<<(allowed[i]-'a'));
+}
+        
+for (auto c:words){
+    bool flag=true;
+    for (auto x:c) {
+        int count=mask&(1<<(x-'a'));
+        if (count==0){
+            flag=false;
+            break;
         }
-        return count;
+    }
+    if(flag){
+        ans++;
+    }
+}
+        
+return ans;
+
     }
 };
