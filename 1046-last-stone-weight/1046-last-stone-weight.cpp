a1619112e7b2670d8e4ca7e27594e70d43d508ed
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-24 19:02:55 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (41.71%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
+         if(stones.size()==1)return stones[0];
         
-//         sort(stones.begin(),stones.end());
-//         reverse(stones.begin(),stones.end());
+        sort(stones.begin(),stones.end());
         
-//         int n=stones.size()-1;
-        
-//         while(stones[1]!=0){
-//             if(stones[0]==stones[1]){
-//                 stones[0]=0;stones[1]=0;    
-//             }else if(stones[0]>stones[1]){stones[0]=abs(stones[1]-stones[0]);stones[1]=0;}
-//             else if(stones[1]>stones[0]){stones[1]=abs(stones[0]-stones[1]);stones[0]=0;}
-        
-//         sort(stones.begin(),stones.end());
-//         reverse(stones.begin(),stones.end());
-//         }        
-        
-//         return stones[0];
-        
-        
-        if(stones.size()==1)return stones[0];
-        
-        sort(stones.begin(),stones.end()); 
-        int p=0;
-        int q=0;
-        int n=stones.size()-1;
-        while(n!=0){
-             p=stones[n];  
-             q=stones[n-1]; 
-            stones.pop_back(); 
-             stones.pop_back();
-            n=n-2;
-            
-            int temp=abs(p-q);
-            // if(temp==0)continue;
-             stones.push_back(temp);
-            n++;
-            sort(stones.begin(),stones.end());
-            
-        }
+        while(stones.size()>1){
+    sort(stones.begin(), stones.end());
+    int x = stones.back(); stones.pop_back();
+    int y = stones.back(); stones.pop_back();
+    if(x!=y){
+        stones.push_back(x-y);
+    }
+}
+
+        if(stones.size()==0)return 0;
         return stones[0];
+        
+    
     }
 };
