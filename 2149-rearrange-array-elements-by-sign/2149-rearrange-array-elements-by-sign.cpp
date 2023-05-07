12e7024d1526a2d5db9d2ce6dfd3756cef187d0d
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-05-07 13:19:17 +0530

    Time: 264 ms (5.70%), Space: 123.8 MB (84.51%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,25 +2,46 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         // sort(nums.begin(),nums.end());
+       
         
-        vector<int>temp;
-        vector<int>temp2;
-        int k=0;int j=0;
         
+//         vector<int>temp;
+//         vector<int>temp2;
+//         int k=0;int j=0;
+        
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]<0)temp.push_back(nums[i]);
+//             if(nums[i]>0)temp2.push_back(nums[i]);
+//         }
+        
+        
+//         nums.clear();
+//         for(int i=0;i<temp.size();i++){
+//             nums.push_back(temp2[j++]);
+//             nums.push_back(temp[k++]);            
+//         }
+        
+        
+//         return nums;
+        
+        vector<int>ans(nums.size(),0);
+        
+        int indp=0;int indn=1;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]<0)temp.push_back(nums[i]);
-            if(nums[i]>0)temp2.push_back(nums[i]);
+            if(nums[i]>0){
+                ans[indp]=nums[i];
+                indp+=2;
+            }
+            if(nums[i]<0){
+                ans[indn]=nums[i];
+                indn+=2;
+            }
         }
         
-        
-        nums.clear();
-        for(int i=0;i<temp.size();i++){
-            nums.push_back(temp2[j++]);
-            nums.push_back(temp[k++]);            
-        }
+        return ans;
         
         
-        return nums;
+        
         
     }
 };
