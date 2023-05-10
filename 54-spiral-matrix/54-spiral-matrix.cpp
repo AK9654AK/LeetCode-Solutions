b162892f3d829a652556d11a364424329ca5d724
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-05-10 14:05:08 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (69.57%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-   int row=matrix.size();
-   int col=matrix[0].size();
-   vector<int> v;
-   int t=0;
-   int b=row-1;
-   int l=0;
-   int r=col-1;
-   int dir=1;
-
-   while(l<=r && t<=b){
-      if(dir==1){
-         for(int i=l;i<=r;i++) v.push_back(matrix[t][i]);
-         t++;
-         dir=2;
-      }
-      else if(dir==2){
-         for(int i=t;i<=b;i++) v.push_back(matrix[i][r]);
-         r--;
-         dir=3;
-      }
-      else if(dir==3){
-         for(int i=r;i>=l;i--) v.push_back(matrix[b][i]);
-         b--;
-         dir=4;
-      }
-      else if(dir==4){
-         for(int i=b;i>=t;i--) v.push_back(matrix[i][l]);
-         l++;
-         dir=1;
-      }
-   }
-   return v; 
-} 
+   vector<int> res;
+    if (matrix.size() == 0) {
+        return res;
+    }
+    
+    int rowBegin = 0;
+    int rowEnd = matrix.size() - 1;
+    int colBegin = 0;
+    int colEnd = matrix[0].size() - 1;
+    
+    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+        // Traverse Right
+        for (int j = colBegin; j <= colEnd; j++) {
+            res.push_back(matrix[rowBegin][j]);
+        }
+        rowBegin++;
+        
+        // Traverse Down
+        for (int j = rowBegin; j <= rowEnd; j++) {
+            res.push_back(matrix[j][colEnd]);
+        }
+        colEnd--;
+        
+        if (rowBegin <= rowEnd) {
+            // Traverse Left
+            for (int j = colEnd; j >= colBegin; j--) {
+                res.push_back(matrix[rowEnd][j]);
+            }
+        }
+        rowEnd--;
+        
+        if (colBegin <= colEnd) {
+            // Traverse Up
+            for (int j = rowEnd; j >= rowBegin; j--) {
+                res.push_back(matrix[j][colBegin]);
+            }
+        }
+        colBegin++;
+    }
+    
+    return res;
+}
 
 };
