4dcc136961b15423265974dfc678d67d40d8e49c
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-05-14 17:35:43 +0530

    Time: 81 ms (24.61%), Space: 27.4 MB (92.44%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -1,9 +1,21 @@
+
+  int r;
+bool comp(vector<int>& a, vector<int>& b)
+{   
+    return (a[r]>b[r]); 
+}
 class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-        for(int i=0;i<score.size();i++)swap(score[i][k],score[i][0]);
-        sort(score.rbegin(),score.rend());
-        for(int i=0;i<score.size();i++)swap(score[i][k],score[i][0]);
+       // need to store k globally so can be used in comp function
+        r=k;
+       // used comparator function for sorting
+        sort(score.begin(),score.end(),comp);
         return score;
-}
+    }
 };
+        // for(int i=0;i<score.size();i++)swap(score[i][k],score[i][0]);
+        // sort(score.rbegin(),score.rend());
+        // for(int i=0;i<score.size();i++)swap(score[i][k],score[i][0]);
+        // return score;
+
