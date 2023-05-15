21b49a089efcce1518c9b26702f7490c60704ef6
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-05-15 16:24:44 +0530

    Time: 6 ms (30.56%), Space: 7.4 MB (88.37%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -11,18 +11,20 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        ListNode* curr=head;
-    while(curr != NULL && curr->next!=NULL) {
-            swap(curr->val ,curr->next->val);
-            curr=curr->next->next;
+      if(!head || !head->next) return head; //If there are less than 2 nodes in the given nodes, then no need to do anything just return the list as it is.
+		
+        ListNode* dummyNode = new ListNode();
+        ListNode* prevNode=dummyNode;
+        ListNode* currNode=head;
+        while(currNode && currNode->next){
+            prevNode->next = currNode->next;
+            currNode->next = prevNode->next->next;
+            prevNode->next->next = currNode;
+            prevNode=currNode;
+            currNode=currNode->next;
         }
-        return head;
+        return dummyNode->next;
     }
-
-        
-        
-  
-    
     // forSwappingValues:ListNode *curr=head;
     //     while(curr != NULL && curr->next!=NULL) {
     //         swap(curr->val ,curr->next->val);
