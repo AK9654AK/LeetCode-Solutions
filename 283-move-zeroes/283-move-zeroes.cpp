e1337f3e0279f76567c49b3504c4040691c77f3d
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-05-17 01:15:26 +0530

    Time: 104 ms (11.37%), Space: 19.2 MB (83.32%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+         int n = nums.size();
+    int count = 0;
+    
+    for (int i = 0; i <nums.size(); i++) {
+        if (nums[i] == 0) {
+            nums.erase(nums.begin() + i);
+            count++;
+            i--; 
+            
+        }
+    }
+    
+    for (int i = 0; i < count; i++) {
+        nums.push_back(0);
+    }
+    
+// 	  for(int i=0,next=0;i<nums.size();i++){
+// 		   if(nums[i]!=0){
+// 			swap(nums[i],nums[next++]);}
+
+//     }
+        
+        
+//         void moveZeroes(vector<int>& nums) {
+//         int n = nums.size();
+//         int i = 0;
+//         int j = 1;
+//         while(j<n){
+//             if(nums[i]==0 && nums[j]!=0){
+//                 swap(nums[i],nums[j]);
+//                 i++;
+//                 j++;
+//             }
+//             else if(nums[i]==0 && nums[j]==0){
+//                 j++;
+//             }
+//             else{
+//                 i++;
+//                 j++;
+//             }
+//         }
+//     }
+// };
+        
+        
+        
+        
+           
+//          void moveZeroes(vector<int>& nums) {
+//         int i=0,j=0;
+//         while(i<nums.size() && j<nums.size()){
+//             if(nums[j]!=0 ){
+//                 swap(nums[i],nums[j]);
+//                 i++;
+//                 j++;
+//             }else j++;
+//         }  
+//     }
+// };
+    }
+};
