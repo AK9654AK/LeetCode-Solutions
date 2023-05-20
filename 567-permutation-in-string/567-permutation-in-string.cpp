ce998c52bf14a5235a296c7f2f04eea5b66f8462
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-05-20 17:53:22 +0530

    Time: 13 ms (44.58%), Space: 7.3 MB (46.24%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int>hash1(26,0);
+          vector<int>hash2(26,0);
+        int n=s1.size(),m=s2.size();
+        
+        for(int i=0;i<n;i++){
+            hash1[s1[i]-'a']++;
+        }
+        int i=0,j=0;
+        while(i<m){
+            hash2[s2[i]-'a']++;
+            if(i>=n) hash2[s2[j++]-'a']--;
+            i++;
+            if(hash1==hash2)return true;
+        }
+        // if(hash1==hash2)return true;
+        return false;
+        
+        
+    }
+};
