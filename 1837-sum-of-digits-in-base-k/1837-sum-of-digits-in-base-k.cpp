a5c527136a537c7a52c2a2f8d3b357386003e97c
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-06-02 19:56:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (54.00%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
@@ -4,9 +4,26 @@ public:
         
         int ans=0;
         while(n){
-            ans+=n%k;
+            ans+=n%k; 
             n/=k;
         }
+        
+//         string convertBase(int n, int k) {
+//     string result = "";
+
+//     while (n > 0) {
+//         int remainder = n % k;
+
+//         char digit = (remainder < 10) ? (remainder + '0') : (remainder - 10 + 'A'); if remainder is greater than 10. It means that it in hexadecimal. 
+
+//         result = digit + result;
+//         n /= k;
+//     }
+
+//     return result;
+// }
+
+        
         return ans;
     }
 };
