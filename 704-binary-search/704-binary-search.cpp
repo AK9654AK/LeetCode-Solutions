eb7357171e9ab16be9a7e55568015c3c0bf599d1
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-06-05 20:29:18 +0530

    Time: 37 ms (78.74%), Space: 27.5 MB (89.01%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -3,19 +3,14 @@ public:
     int search(vector<int>& nums, int target) {
     
         int l=0;
-        int h=nums.size()-1;
-        
-          while(l<=h){
+        int h=nums.size();
+          while(l<h){
               
               int mid=l+(h-l)/2;
               
               if(nums[mid]==target)return mid;
-              if(nums[l]<nums[mid]){
-                  if(target>=nums[l] and target<nums[mid])h=mid-1;else l=mid+1;
-              }
-              else{
-                  if(target<=nums[h] and target>nums[mid])l=mid+1;else h=mid-1;
-              }
+              else if(nums[mid]>target)h=mid;
+              else if(nums[mid]<target)l=mid+1;
           }
         
         return -1;
