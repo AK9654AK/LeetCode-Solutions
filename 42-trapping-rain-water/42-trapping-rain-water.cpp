1160cf887fd1b15c9d630484998fbe550c681a0a
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-06-20 17:31:49 +0530

    Time: 17 ms (59.07%), Space: 20.5 MB (18.65%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -7,11 +7,12 @@ public:
         vector<int> left(n , 0);
         vector<int> right(n, 0);
         for(int i = 0; i< n; i++){
-            if(max_l < height[i]) max_l = height[i];
+            max_l = max(max_l,height[i]);
             left[i] = max_l;      // finding maximum on left side for each index 
         }
         for(int i = n -1; i>= 0; i--){
-            if(max_r < height[i]) max_r = height[i];
+            // if(max_r < height[i])
+                max_r = max(max_r,height[i]);
             right[i] = max_r;// finding maximum on left side for each index 
         }
         int ans = 0;
