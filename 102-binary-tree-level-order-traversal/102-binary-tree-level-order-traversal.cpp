55f504421e7ff106697d64015a9cf5b4320775dd
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-06-26 15:23:16 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (43.53%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -12,22 +12,55 @@
 class Solution {
 public:
     vector<vector<int>>levelOrder(TreeNode* root){
-        vector<vector<int>>res;
+        
+        vector<vector<int>>ans;
         queue<TreeNode*>q;
+        if(root==NULL)return ans;
         q.push(root);
-        if(root==NULL)return res;
+        
         while(!q.empty()){
-            vector<int>ans;
-            int size=q.size();
+            long long int size=q.size();
+            vector<int>res;
             for(int i=0;i<size;i++){
-                TreeNode *node=q.front();
+                TreeNode *tr=q.front();
                 q.pop();
-                if(node->left)q.push(node->left);
-                if(node->right)q.push(node->right);
-                ans.push_back(node->val);
+                if(tr->left)q.push(tr->left);
+                if(tr->right)q.push(tr->right);
+                res.push_back(tr->val);
             }
-            res.push_back(ans); 
+            
+            ans.push_back(res);  
+            
+            
         }
-        return res;
+        
+        
+        
+    
+        
+        
+        return ans;
+        
+        
+        
+        
+        
+        // vector<vector<int>>res;
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // if(root==NULL)return res;
+        // while(!q.empty()){
+        //     vector<int>ans;
+        //     int size=q.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode *node=q.front();
+        //         q.pop();
+        //         if(node->left)q.push(node->left);
+        //         if(node->right)q.push(node->right);
+        //         ans.push_back(node->val);
+        //     }
+        //     res.push_back(ans); 
+        // }
+        // return res;
     }
 };
