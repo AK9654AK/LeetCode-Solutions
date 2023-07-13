62add9c842cd4490a3b53be4c5e68f3d6377eac7
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-07-13 22:08:39 +0530

    Time: 21 ms (85.18%), Space: 13.4 MB (75.26%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+   bool dfs(vector<int> * list,vector<int> &visited,int source){
+    if(visited[source])
+    {
+    return false;
+
+    }
+    visited[source]=true;
+    for(auto child: list[source]){
+        if(visited[child]!=2){
+
+        
+        if(dfs(list,visited,child)==false){
+            return false;
+        }
+        }
+    }
+    visited[source]=2;
+    return true;
+}
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+     int n=numCourses;
+     vector<int> list [n];
+     for(int i=0;i<prerequisites.size();i++){
+             list[prerequisites[i][0]].push_back(prerequisites[i][1]);
+         
+     }   
+     vector<int> visited(n+1,false);
+     for(int i=0;i<n;i++){
+         if(!visited[i]){
+             if(dfs(list,visited,i)==false){
+                 return false;
+             }
+         }
+     }
+     return true;
+    }
+};
