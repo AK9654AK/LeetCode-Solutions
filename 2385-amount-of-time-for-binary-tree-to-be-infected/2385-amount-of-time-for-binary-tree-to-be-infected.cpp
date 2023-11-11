815f34910d9bf11c573aee751499ff2b1f717273
Abhishek K <82321076+AK9654AK@users.noreply.github.com>
2023-11-11 10:41:36 +0530

    Time: 381 ms (47.23%), Space: 183.7 MB (22.15%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        unordered_map<int,vector<int>> m;
+        unordered_map<int,int> vis;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())//adding neighbours
+        {
+            TreeNode* t=q.front();
+            q.pop();
+            if(t->left)
+            {
+                m[t->val].push_back(t->left->val);
+                m[t->left->val].push_back(t->val);
+                q.push(t->left);
+            }
+            if(t->right)
+            {
+                m[t->val].push_back(t->right->val);
+                m[t->right->val].push_back(t->val);
+                q.push(t->right);
+            }
+        }
+        queue<pair<int,int>> pq;
+        pq.push({start,0});
+        int res=0;
+        vis[start]=1;
+        while(!pq.empty())
+        {
+            auto p=pq.front();
+            pq.pop();
+            res=max(res,p.second);
+            for(auto x:m[p.first])
+            {
+                if(vis[x]==0)
+                {
+                    vis[x]=1;
+                    pq.push({x,p.second+1});
+                }
+            }
+        }
+        return res;
+    }
+};
