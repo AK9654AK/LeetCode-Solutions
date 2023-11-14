f9e87b32f6a999bf18f94ee040aa6d2daae9e8c9
Abhishek K <82321076+AK9654AK@users.noreply.github.com>
2023-11-14 20:18:51 +0530

    Time: 14 ms (17.92%), Space: 16.2 MB (95.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-   vector<int> majorityElement(vector<int>& nums) {
-		// choose candidate integers
-		int cand1 = 0, count1 = 0;
-		int cand2 = 0, count2 = 0;
-		for (int num : nums) {
-			// count matches
-			if (num == cand1) count1++;
-			else if (num == cand2) count2++;
-			// reselect candidate if needed, ensuring that they're distinct
-			else if (count1 <= 0 and num != cand2) {
-				cand1 = num;
-				count1 = 1;
-			}
-			else if (count2 <= 0 and num != cand1) {
-				cand2 = num;
-				count2 = 1;
-			}
-			// decerement count if no matches or reslection
-			else {
-				count1--;
-				count2--;
-			}
-		}
+    vector<int> majorityElement(vector<int>& nums) {
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
 
-		// check candidates
-		count1 = 0;
-		count2 = 0;
-		for (int num : nums) {
-			if (num == cand1) count1++;
-			else if (num == cand2) count2++;
-		}
+    for (int num : nums) {
+        if (num == candidate1) {
+            count1++;
+        } else if (num == candidate2) {
+            count2++;
+        } else if (count1 == 0) {
+            candidate1 = num;
+            count1 = 1;
+        } else if (count2 == 0) {
+            candidate2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
+        }
+    }
 
-		// create output
-		vector<int> out;
-		if (count1 > nums.size() / 3) out.push_back(cand1);
-		if (count2 > nums.size() / 3) out.push_back(cand2);
-		return out;
-	}
+    count1 = 0;
+    count2 = 0;
+
+    for (int num : nums) {
+        if (num == candidate1) {
+            count1++;
+        } else if (num == candidate2) {
+            count2++;
+        }
+    }
+
+    std::vector<int> result;
+    if (count1 > nums.size() / 3) {
+        result.push_back(candidate1);
+    }
+    if (count2 > nums.size() / 3) {
+        result.push_back(candidate2);
+    }
+
+    return result;
+}
 };
